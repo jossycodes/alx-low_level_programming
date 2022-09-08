8acb3586a4f7c16cf1307c88b959dc1c0bca1789
jossycodes <josiahadeniyi1@gmail.com>
2022-09-08 16:49:38 -0700

    Edited 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,9 @@
-#include <unistd.h> 
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <unistd.h>
 
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
